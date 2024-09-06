9071a2ffa8c60a1299a2e305c226656db960b36c
Okibuyan <boika.dashdeleg@gmail.com>
2024-09-06 10:19:50 +0800

    .

--- src/components/pages/SinglePost.jsx
+++ src/components/pages/SinglePost.jsx
@@ -1,13 +1,30 @@
+import BlogPostHomepage from "../blog-post/ArticleCardHomepage";
+import BlogPost, { ArticleCard } from "../blog-post/ArticleCard";
+import { useEffect, useState } from "react";
 import { Footer } from "../footer/Footer";
 import { Header } from "../header/Header";
+import { useRouter } from "next/router";
 
 export default function SinglePostPage() {
+  const router = useRouter();
+
+  const [article, setArticle] = useState([]);
+  const fetchData = () => {
+    fetch(`https://dev.to/api/articles/${router.query.id}`)
+      .then((response) => response.json())
+      .then((data) => setArticle(data));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(article);
   return (
     <div className="flex flex-col gap-[100px]">
       <Header />
       <div>
         <div className="text-[36px] text-[#181A2A] font-semibold">
-          article.title
+          {article?.title}
         </div>
       </div>
 
